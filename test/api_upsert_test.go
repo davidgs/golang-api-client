53379ea39eaf5fce583fa20c8b7eab858d63ed4c
David G. Simmons <santafen@mac.com>
2023-04-13 08:37:43 -0400

    git id changes

--- test/api_upsert_test.go
+++ test/api_upsert_test.go
@@ -13,7 +13,7 @@ import (
 	"context"
 	"testing"
 
-	openapiclient "github.com/GIT_USER_ID/golang-api-client"
+	openapiclient "github.com/davidgs/golang-api-client"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
